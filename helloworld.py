02ba2aad2e86ac2787754fe6306283c8a10bba14
Dumfing <dumpling.d.m@gmail.com>
2017-01-21 14:59:45 -0500

    helloworld update

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_ask import Ask, statement
-
+from getData import getData
+from urllib.request import urlopen
+url = urlopen("https://data.michigan.gov/OData.svc/aiht-57sm")
+tripInfo = getData(url)
 app = Flask(__name__)
 ask = Ask(app, '/')
 
@@ -9,6 +12,11 @@ def hello():
     #speech_text = "Hello %s" % firstname
     #return statement(speech_text).simple_card('Hello', speech_text)
     return statement("Minya so short")
-
+@ask.intent('RandomTrip')
+def randTrip():
+    return statement("Trip placeholder")
+@ask.intent('RandomTripDist', convert={'MaxDist':int})
+def randTripDist(maxDist):
+    return statement(str(maxDist))
 if __name__ == '__main__':
     app.run()
