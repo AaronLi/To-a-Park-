3658004344c8abec9557f04095299f66d3aebfeb
Dumfing <dumpling.d.m@gmail.com>
2017-01-21 13:24:55 -0500

    addedGetData

---
+++ getData.py
@@ -0,0 +1,22 @@
+def getData(f):
+    "f is the opened file"
+    getData = []
+    #ali is the entire file
+    ali = f.read().decode().split("<entry>")
+
+    for entry in ali:#looking for
+        linedata = []
+
+        for line in entry.split("\n") :
+            if line[:4] == "<id>":
+                continue
+            data = line.strip(">").split(">")
+            if len(data) == 1 or len(data) > 2:
+                continue
+            data2 = []
+            for k in data:
+                data2 = data2 + k.split("<")
+            if data2[-1][3:] != "__id":
+                 linedata.append(data2[-1][3:]+" " + data2[-2].strip())
+        getData.append(linedata)
+    return getData
